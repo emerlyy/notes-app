a8632be0b1dfd36d149091db1ba159528573d884
Emerly <vadim200339@gmail.com>
2025-02-21 14:23:22 +0200

    fix: Fixed settings page appearance

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,13 +1,16 @@
 import SettingsControls from "@/components/SettingsControls/SettingsControls";
 import SettingsList from "@/components/SettingsList/SettingsList";
 import DashboardLayout from "@/components/ui/DashboardLayout/DashboardLayout";
+import MainLayout from "../MainLayout";
 
 export default function SettingsPage() {
   return (
-    <DashboardLayout
-      mainPanel="right"
-      leftPanel={<SettingsList />}
-      rightPanel={<SettingsControls />}
-    />
+    <MainLayout title="Settings">
+      <DashboardLayout
+        mainPanel="right"
+        leftPanel={<SettingsList />}
+        rightPanel={<SettingsControls />}
+      />
+    </MainLayout>
   );
 }
