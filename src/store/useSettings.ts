480a970d1352d79e822fb953dd0cde4b90fff431
Emerly <vadim200339@gmail.com>
2025-02-21 13:01:00 +0200

    feat: Created settings store

---
+++ src/store/useSettings.ts
@@ -0,0 +1,32 @@
+import { create } from "zustand";
+
+export type SettingType = "color-theme" | "font-theme";
+export type ColorTheme = "dark" | "light" | "system";
+export type FontTheme = "sans-serif" | "serif" | "monospace";
+
+type SettingsState = {
+  active: SettingType;
+  color: ColorTheme;
+  font: FontTheme;
+};
+
+const initialState: SettingsState = {
+  active: "color-theme",
+  color: "light",
+  font: "sans-serif",
+};
+
+type SettingsActions = {
+  setActive: (setting: SettingType) => void;
+  setColor: (color: ColorTheme) => void;
+  setFont: (font: FontTheme) => void;
+};
+
+export type SettingsStore = SettingsState & SettingsActions;
+
+export const useSettings = create<SettingsStore>((set) => ({
+  ...initialState,
+  setActive: (setting) => set({ active: setting }),
+  setColor: (color) => set({ color: color }),
+  setFont: (font) => set({ font: font }),
+}));
